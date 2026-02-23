103e6f4de3cb4d491f08ab04a134559ba6d87766
yus <swiftcase@gmail.com>
2026-02-23 23:47:42 +0800

    Update constants.js

--- static/js/utils/constants.js
+++ static/js/utils/constants.js
@@ -1,18 +1,20 @@
 // static/js/utils/constants.js
-export const HEXEL_SIZE = 24.0; // Force float with .0
+export const HEXEL_SIZE = 24.0;
 
-// Force ALL derived values to be floats by using .0 in calculations
-export const H_STEP = HEXEL_SIZE * 2.0;           // 48.0 - horizontal spacing
-export const V_STEP = HEXEL_SIZE * 1.73205080757; // 41.569... - already float
+export const H_STEP = HEXEL_SIZE * 2.0;
+export const V_STEP = HEXEL_SIZE * 1.73205080757;
 
 export const SQRT3 = 1.73205080757;
 export const TAU = 6.28318530718;
 
-// Grid colors as vec3 components
+// GRID_COLOR as separate components for shaders
 export const GRID_COLOR_R = 0.784;
 export const GRID_COLOR_G = 0.576;
 export const GRID_COLOR_B = 0.824;
 
-// Line widths as floats
+// Or as a hex string for JavaScript
+export const GRID_COLOR = '#c893d2';  // ← ADD THIS!
+export const DEFAULT_POINT_COLOR = '#ffaa66';
+
 export const GRID_LINE_WIDTH = Math.max(1.0, HEXEL_SIZE / 24.0);
 export const POINT_SIZE = HEXEL_SIZE / 3.0;
