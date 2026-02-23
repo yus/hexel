ec75ec610589be7d3586723b8a8c0809fe232510
yus <swiftcase@gmail.com>
2026-02-23 13:04:20 +0800

    Update webgl-renderer.js

--- static/js/core/webgl-renderer.js
+++ static/js/core/webgl-renderer.js
@@ -41,14 +41,10 @@ export class HexelRenderer {
         // === GRID SHADER (your existing one with debug) ===
         const gridVS = `
             attribute vec2 a_position;
-            uniform vec2 u_resolution;
-            uniform vec2 u_offset;
-            uniform float u_scale;
             
             void main() {
-                vec2 worldPos = a_position * u_scale + u_offset;
-                vec2 screenPos = worldPos / u_resolution * 2.0 - 1.0;
-                gl_Position = vec4(screenPos * vec2(1, -1), 0, 1);
+                // a_position is already in clip space (-1 to 1)
+                gl_Position = vec4(a_position, 0, 1);
             }
         `;
         
