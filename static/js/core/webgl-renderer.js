28f05469a1967c5cceb89494226398f9bd027c30
yus <swiftcase@gmail.com>
2026-02-24 00:12:00 +0800

    Update webgl-renderer.js

--- static/js/core/webgl-renderer.js
+++ static/js/core/webgl-renderer.js
@@ -54,16 +54,21 @@ export class HexelRenderer {
                 gl_Position = vec4(screenPos * vec2(1, -1), 0, 1);
             }
         `;
-        
+
+        const formatGLSLFloat = (num) => {
+            return num.toString().includes('.') ? num.toString() : num.toString() + '.0';
+        };
+
         const gridFS = `
             precision highp float;
+            
             uniform vec2 u_resolution;
             uniform vec2 u_offset;
             uniform float u_scale;
             uniform float u_opacity;
             
-            const float H_STEP = ${H_STEP}.0;
-            const float V_STEP = ${V_STEP};
+            const float H_STEP = ${formatGLSLFloat(H_STEP)};
+            const float V_STEP = ${formatGLSLFloat(V_STEP)};
             const float SQRT3 = ${SQRT3};
             const vec3 GRID_COLOR = vec3(${parseInt(GRID_COLOR.slice(1,3),16)/255}, ${parseInt(GRID_COLOR.slice(3,5),16)/255}, ${parseInt(GRID_COLOR.slice(5,7),16)/255});
             
