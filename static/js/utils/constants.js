d969ea1b3bc45e78cda8cb155972ef83c2791abe
yus <swiftcase@gmail.com>
2026-02-23 23:20:48 +0800

    Update constants.js

--- static/js/utils/constants.js
+++ static/js/utils/constants.js
@@ -1,12 +1,16 @@
-export const HEXEL_SIZE = 24;
-export const H_STEP = HEXEL_SIZE * 2;
-export const V_STEP = HEXEL_SIZE * Math.sqrt(3);
+// static/js/utils/constants.js
+export const HEXEL_SIZE = 24; // Base unit - everything derives from this!
 
-export const GRID_COLOR = '#b388ff';
-export const DEFAULT_COLOR = '#ffaa66';
+export const H_STEP = HEXEL_SIZE * 2;           // 48 - horizontal spacing
+export const V_STEP = HEXEL_SIZE * 1.7320508;    // ~41.569 - vertical spacing (sqrt(3) * size)
 
-export const ZOOM_CONFIG = [
-    { max: 0.5, horiz: { alpha: 0.15, width: 0.3 }, diag: { alpha: 0.1, width: 0.2 } },
-    { max: 1.0, horiz: { alpha: 0.15, width: 0.3 }, diag: { alpha: 0.1, width: 0.3 } },
-    { max: Infinity, horiz: { alpha: 0.15, width: (s) => 0.5 / s }, diag: { alpha: 0.1, width: (s) => 0.5 / s } }
-];
+export const SQRT3 = 1.73205080757;
+export const TAU = 6.28318530718;
+
+// Grid colors
+export const GRID_COLOR = '#c893d2';
+export const DEFAULT_POINT_COLOR = '#ffaa66';
+
+// Line widths based on HEXEL_SIZE
+export const GRID_LINE_WIDTH = Math.max(1, HEXEL_SIZE / 24);
+export const POINT_SIZE = HEXEL_SIZE / 3;
